22b1bcaf3f39e1347762cff32505c74d3d8a83aa
Hazel Eythorsdottir <hazeleythorsdottir@gmail.com>
2021-12-15 18:30:04 -0500

    Add config.h functionality

--- source/modules/log/log.cpp
+++ source/modules/log/log.cpp
@@ -9,11 +9,15 @@ namespace
 log_m::log_m(rogeng* re) : 
     re(re) 
 {
+#ifdef LOG_TIME
     start_time = std::chrono::high_resolution_clock::now();
+#endif // LOG_TIME
 }
 
+#ifdef LOG_TIME
 std::chrono::milliseconds log_m::get_time() 
 {
     auto current_time = std::chrono::high_resolution_clock::now();
     return std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time);
 }
+#endif // LOG_TIME
--- source/modules/log/log.h
+++ source/modules/log/log.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <config.h>
+
 #include <iostream>
+
+#ifdef LOG_TIME
 #include <chrono>
+#endif // LOG_TIME
 
 class rogeng;
 
@@ -13,18 +18,24 @@ public:
     template<typename ...Args>
     void log(Args && ...args)
     {
+#ifdef LOG_TIME
         std::cout << "[" << get_time().count() << "] ";
+#endif // LOG_TIME
         (std::cout << ... << args);
     }
     
     template<typename ...Args>
     void print(Args && ...args)
     {
+#ifdef LOG_TIME
         std::cout << "[x] ";
+#endif // LOG_TIME
         (std::cout << ... << args);
     }
 private:
     rogeng* re;
+#ifdef LOG_TIME
     std::chrono::milliseconds get_time();
+#endif // LOG_TIME
 
 };
